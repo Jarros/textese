65e9bf44ed5c8115f97f8fc54d0dfa67766b0d69
Jaroslav <jarros97@gmail.com>
2020-08-09 21:35:05 +0200

    Add files via upload

---
+++ dictionary.h
@@ -0,0 +1,146 @@
+const char* dict[256][8]
+{
+	"d","the", _ _ _ _ _ _
+	"wd","would", _ _ _ _ _ _
+	//" d","'d", _ _ _ _ _ _
+	//" s","s", _ _ _ _ _ _
+	"s","is", _ _ _ _ _ _
+	"n","and", _ _ _ _ _ _
+	"dat","that", _ _ _ _ _ _
+	"dis","this", _ _ _ _ _ _
+	"r","are", _ _ _ _ _ _
+	" r","'re", _ _ _ _ _ _
+	"s","is", _ _ _ _ _ _
+	"u","you", _ _ _ _ _ _
+	"tink","think", _ _ _ _ _ _
+	"ting","thing", _ _ _ _ _ _
+	"y","why", _ _ _ _ _ _
+	"ye","yeah","yes","yep", _ _ _ _
+	"f","for", _ _ _ _ _ _
+	"prob","probably", _ _ _ _ _ _
+	"w","with", _ _ _ _ _ _
+	"bc","because", _ _ _ _ _ _
+	"bout","about", _ _ _ _ _ _
+	"o","or", _ _ _ _ _ _
+	"ppl","people", _ _ _ _ _ _
+	"jk-","jok(e)-","just kidding", _ _ _ _ _
+	"cm-","com(e)-", _ _ _ _ _ _
+	"mb","maybe", _ _ _ _ _ _
+	"rn","right now", _ _ _ _ _ _
+	"rnd","random", _ _ _ _ _ _
+	"rly","really", _ _ _ _ _ _
+	"sry","sorry", _ _ _ _ _ _
+	"sm","some", _ _ _ _ _ _
+	"smth","something", _ _ _ _ _ _
+	"wt","what", _ _ _ _ _ _
+	"ll","will", _ _ _ _ _ _
+	"t","to", _ _ _ _ _ _
+	"j","just", _ _ _ _ _ _
+	"b","be", _ _ _ _ _ _
+	//"b-","be-", _ _ _ _ _ _
+	"vr","very", _ _ _ _ _ _
+	"evth","everything", _ _ _ _ _ _
+	"dif-","different-","difference-", _ _ _ _ _
+	"alw","always", _ _ _ _ _ _
+	"acly","actually", _ _ _ _ _ _
+	"cd","could", _ _ _ _ _ _
+	"kinda","kind of", _ _ _ _ _ _
+	"lang-","language-", _ _ _ _ _ _
+	"civ-","civilization-", _ _ _ _ _ _
+	"der","there", _ _ _ _ _ _
+	"tow-","toward-", _ _ _ _ _ _
+	"lil-","little-", _ _ _ _ _ _
+	"thx-","thanks","thank-", _ _ _ _ _
+	"thru","through", _ _ _ _ _ _
+	"lvl-","level-", _ _ _ _ _ _
+	"dan","than", _ _ _ _ _ _
+	"den","then", _ _ _ _ _ _
+	"tho","though", _ _ _ _ _ _
+	"aft","after", _ _ _ _ _ _
+	"mk","make", _ _ _ _ _ _
+	"tm","time", _ _ _ _ _ _
+	"qt","quite", _ _ _ _ _ _
+	"sr","sure", _ _ _ _ _ _
+	"td","today", _ _ _ _ _ _
+	"v","have", _ _ _ _ _ _
+	"hv-","hav(e)-", _ _ _ _ _ _
+	"gen-","general-","generation-", _ _ _ _ _
+	"pop-","population-", _ _ _ _ _ _
+	"oth-","other-", _ _ _ _ _ _
+	"qn-","question-", _ _ _ _ _ _
+	"ano","another", _ _ _ _ _ _
+	"lit-","literal-", _ _ _ _ _ _
+	"m-","my-","mine-", _ _ _ _ _
+	"-lk-","-like-", _ _ _ _ _ _
+	"ao","also", _ _ _ _ _ _
+	"-ev-","-even-", _ _ _ _ _ _
+	"-mr-","-more-", _ _ _ _ _ _
+	"further-","fuder-", _ _ _ _ _ _
+	"aldy","already", _ _ _ _ _ _
+	"nt","not", _ _ _ _ _ _
+	"eng","english", _ _ _ _ _ _
+	"oth","other", _ _ _ _ _ _
+	"hist","history", _ _ _ _ _ _
+	"-in-","-ing-", _ _ _ _ _ _
+	"kno","know", _ _ _ _ _ _
+	"ably","absolutely", _ _ _ _ _ _
+	"dnt","dont", _ _ _ _ _ _
+	"pol","politic-","political-", _ _ _ _ _
+	"acc-","according","account-", _ _ _ _ _
+	"vrt-","virtual-", _ _ _ _ _ _
+	"ag-","again", _ _ _ _ _ _
+	"bt","but", _ _ _ _ _ _
+	"aply","apparently", _ _ _ _ _ _
+	"tm-","time-", _ _ _ _ _ _
+	"fr","from", _ _ _ _ _ _
+	"nvr-","never-", _ _ _ _ _ _
+	"net-","internet-", _ _ _ _ _ _
+	"phi-","philosophy","philosophical","philosoph-", _ _ _ _
+	"phys","physics","physical", _ _ _ _ _
+	"el-","else-", _ _ _ _ _ _
+	"al-","all-", _ _ _ _ _ _
+
+	"ctrl-", "control-", _ _ _ _ _ _
+	"alt-", "alternate-", "alternative-", _ _ _ _ _
+	"md-", "made-", _ _ _ _ _ _
+
+	"mem", "memory(-ies)", _ _ _ _ _ _
+
+	"-hr-","-here-", _ _ _ _ _ _
+	"alm","almost", _ _ _ _ _ _
+	"lng","long", _ _ _ _ _ _
+	"wnna","want to", _ _ _ _ _ _
+	"gnna","going to", _ _ _ _ _ _
+	"wer","where", _ _ _ _ _ _
+	"bsly","basically", _ _ _ _ _ _
+	"dose","those", _ _ _ _ _ _
+	"wen","when", _ _ _ _ _ _
+	"thot","thought", _ _ _ _ _ _
+	"obv-","obvious-", _ _ _ _ _ _
+	"cur-","current-", _ _ _ _ _ _
+	"bro-","brother-", _ _ _ _ _ _
+	"sis-","sister-", _ _ _ _ _ _
+	"fam","family", _ _ _ _ _ _
+	"fams","families", _ _ _ _ _ _
+	"gt","got", _ _ _ _ _ _
+	"mn","many", _ _ _ _ _ _
+	"esp","especially", _ _ _ _ _ _
+	"bd","bad", _ _ _ _ _ _
+	"gd","good", _ _ _ _ _ _
+	"fok-","fuck-", _ _ _ _ _ _
+	"ex-","example-", _ _ _ _ _ _
+	"vol-","volume-", _ _ _ _ _ _
+	"dem-","them-", _ _ _ _ _ _
+	"-in","-ing", _ _ _ _ _ _
+	"-und-","-under-", _ _ _ _ _ _
+	"-ov-", "-over-", _ _ _ _ _ _
+	"-fren-", "-friend-", _ _ _ _ _ _
+	"g-", "go-", _ _ _ _ _ _
+	"rev", "reverse", _ _ _ _ _ _
+	"-in","-ing", _ _ _ _ _ _
+	"dngr-","danger-", _ _ _ _ _ _
+	"ctrl-","control-", _ _ _ _ _ _
+	"emo-", "emotion-", _ _ _ _ _ _
+	"grnd", "ground-", _ _ _ _ _ _
+
+};
---
+++ textese.cpp
@@ -0,0 +1,227 @@
+﻿
+#include <iostream>
+#include <sstream>
+#include <fstream>
+
+/*
+#include "Windows.h"
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+#include <locale>  
+#include <stack>
+#include <list>
+#include <vector>
+#include <array>
+#include <set>
+*/
+
+#include "dictionary.h"
+
+using namespace std;
+
+std::string txt;
+typedef unsigned char byte;
+#define skip if(false)
+#define _ nullptr,
+
+
+
+/*
+
+	(*)		= optional (duplicate w n wo, wo s prioritized)
+	*-		= open endin (input endin copied t output endin)
+	-*		= open begginin (input begginin copied t output begginin)
+	(column (input) priority: left>>right)
+	(row (word) priority: up>>down) (so longer words/phrases t put in d beginin)
+
+	TODO: parse titles / namins (eg words dat begin w uppercase / enclosed in brackets) n skip replacin dem
+
+
+*/
+
+void Replace(std::string& txt,
+	const std::string& oldStr,
+	const std::string& newStr)
+{
+	std::string::size_type pos = 0u;
+	while ((pos = txt.find(oldStr, pos)) != std::string::npos) {
+		txt.replace(pos, oldStr.length(), newStr);
+		pos += newStr.length();
+	}
+}
+
+bool IsLetter(char c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return true;
+	return false;
+}
+
+bool IsUppercaseLetter(char c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return true;
+	return false;
+}
+bool IsLowercaseLetter(char c)
+{
+	if (c >= 'a' && c <= 'z')
+		return true;
+	return false;
+}
+
+void ParseInput()
+{
+	int l=txt.length();
+	
+	{
+		int i = 0;
+		int n = 0;
+
+		while (i < l + n)
+		{
+			if (IsUppercaseLetter(txt[i]))
+			{
+				txt[i] -= ('A' - 'a');
+				if (i == 0 || !IsLetter(txt[i - 1]))
+				{
+					txt.insert(i, "^");// txt[i] = '+';
+					n++; i++;
+				}
+			}
+			i++;
+		}
+	}
+	for (int i = 0; i < 96/*256*/; i++)
+	{
+		if (dict[i][0])
+		{
+
+			std::string sO = dict[i][0];
+			bool flag_begin = false;
+			bool flag_end = false;
+			int sO_endpos = sO.length() - 1;
+
+			if (sO[0] == '-')
+			{
+				flag_begin = true;
+				sO = sO.substr(1, sO_endpos);
+			}
+			if (sO[sO_endpos] == '-')
+			{
+				flag_end = true;
+				sO = sO.substr(0, sO_endpos - 1);
+			}
+
+			for (int k = 1; k < 8; k++)
+			{
+				if (dict[i][k])
+				{
+					std::string s = dict[i][k];
+					int endpos = s.length() - 1;
+					if (s[0] == '-')
+					{
+						if (s[endpos] == '-')// && !IsLetter(txt[i - 1]) && !IsLetter(txt[i + s.length() + 1]))
+						{
+							Replace(txt, s.substr(1, endpos - 1), sO);
+						}
+						else
+						{
+							Replace(txt, s.substr(1, endpos), sO);
+						}
+					}
+					else if (s[endpos] == '-')
+					{
+						Replace(txt, s.substr(0, endpos - 1), sO);
+					}
+					else
+					{
+						Replace(txt, s, sO);
+					}
+				}
+				else
+					break;
+			}
+		}
+	}
+	
+	skip
+	for (int i = 0; i < txt.length(); i++)
+		if (txt[i] == '^') {
+			txt.erase(i);
+			if (IsLowercaseLetter(txt[i])) {
+				txt[i]+=('A' - 'a');
+			}
+		}
+
+	l = txt.length();
+	{
+		int i = 0;
+		int n = 0;
+
+		while (i < l + n)
+		{
+			if (txt[i]=='^')
+			{
+
+				txt.erase(i, 1);
+				txt[i] += ('A' - 'a');
+				n--;
+			}
+			else
+				i++;
+		}
+	}
+
+
+	skip
+	for (int i = 0; i < txt.length(); i++)
+		if(!IsLetter(txt[i]))
+			txt[i] = '+';
+}
+
+
+void Save()
+{
+	/*
+	std::ofstream myFile("out.txt");
+
+	myFile.close();
+	*/
+}
+
+void Load()
+{
+	char* in;
+
+	std::ifstream myFile("input.txt");
+
+	std::string line;
+	//while (
+	//std::getline(myFile, line);
+		//)
+	{
+
+	}
+
+	std::stringstream strStream;
+	strStream << myFile.rdbuf(); //read the file
+	txt = strStream.str(); //txt holds the content of the file
+
+	myFile.close();
+
+
+}
+
+
+int main()
+{
+
+	Load();
+	ParseInput();
+	cout << txt;
+	int k; cin >> k;
+
+}
